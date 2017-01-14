c9dd852c5b7f3fdec15214837c68a93ee0d987b0
Joyce <geojoyce934@gmail.com>
2017-01-14 11:09:49 +0530

    roundoff a float value

---
+++ roundoff.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+int main()
+{
+  float a;
+  cin>>a;
+ 
+  cout<<round(a)<<endl;
+  int b;
+  b=(int)(a+0.5);
+  cout<<endl<<"Other way "<<b<<endl;
+  
+}
